97fe397d9ff79b483e11d881b974029036b4ff3a
TibovdbVives <tibo.vandenbroeck@student.vives.be>
2024-04-12 16:13:09 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Project_Keyboard_Tester
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
